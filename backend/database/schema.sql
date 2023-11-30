3fd34818079d4aa4b1d06b60e359f5e0eaf15a96
iNaeRoZ <naeroz@outlook.fr>
2023-11-30 16:20:28 +0100

    finition schema sql

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -1,13 +1,14 @@
 create table bossouls (
   id int unsigned primary key auto_increment not null,
   name varchar(255) not null,
-  description VARCHAR(255) not null,
   image VARCHAR(255),
   HP VARCHAR(255),
-  Souls VARCHAR(255)
+  Souls VARCHAR(255),
+  description VARCHAR(255) not null
 );
 
 create table enemygame (
   id int unsigned primary key auto_increment not null,
-  name varchar(255) not null
+  dlc VARCHAR(255) not null,
+  Requis VARCHAR(255) not null
 );
