89fab12b484bdf691e4f1f9dde2ebcf3faf08c05
iNaeRoZ <naeroz@outlook.fr>
2023-11-30 16:02:10 +0100

    mise en place table

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -1,4 +1,13 @@
-create table item (
+create table bossouls (
   id int unsigned primary key auto_increment not null,
-  title varchar(255) not null
+  name varchar(255) not null,
+  description VARCHAR(255) not null,
+  image VARCHAR(255),
+  HP VARCHAR(255),
+  Souls VARCHAR(255)
+);
+
+create table enemygame (
+  id int unsigned primary key auto_increment not null,
+  name varchar(255) not null
 );
